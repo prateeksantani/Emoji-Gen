8054762c0e20e634d8c1a65c74f540e24b0b7aa5
Prateek Santani <prateeksantani98@gmail.com>
2025-02-25 11:23:00 +0530

    Update background-beams-with-collision.tsx
    unused ref remoed

--- components/ui/background-beams-with-collision.tsx
+++ components/ui/background-beams-with-collision.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
+
 "use client";
 import { cn } from "@/lib/utils";
 import { motion, AnimatePresence } from "framer-motion";
